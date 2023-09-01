c7eddc6ed5b86e773c3e21a2b53e4fe89ec7d305
https://github.com/Khizzii/Saylani.git <khizar.faazal@gmail.com>
2023-09-01 22:29:08 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,46 @@
+// CHAPTER 1
+
+// TASK 1
+// alert("Assalamualaikum")
+
+// TASK 2
+// alert("Error! Please Enter A Valid Password.")
+
+// TASK 3
+// alert("Welcome To JS Land... \nHappy Coding!")
+
+// TASK 4
+// alert("Welcome to JS Land...")
+// alert('Happy coding! \n▢ Prevent this page from creating additional dialogs.')
+
+
+// CHAPTER 2
+
+// TASK 1 And 2
+// var name='Khizar'
+// var name2='Iqbal'
+// console.log( name + name2)
+
+// TASK 3
+// alert('Hello World')
+
+// TASK 4
+// alert('Khizar Iqbal')
+// alert('20 Years Old')
+// alert('Certified Mobile Application Development')
+
+// TASK 5
+// alert('PIZZA \nPIZZ \nPIZ \nPI \nP')
+
+// TASK 6
+// var email='abcdef@gmail.com'
+// alert('my email address is '+email)
+
+// TASK 7
+// var book='I Am Trying To Learn From The Book A Smarter Way To Learn JavaScript'
+// alert(book)
+
+
+
+
+
